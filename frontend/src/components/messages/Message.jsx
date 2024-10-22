776e2a5711e2393b46ee28755ede1e07d72f01f0
蘇泰儒 <terry_su@sutairudebijixingdiannao.local>
2024-10-22 08:01:54 +0800

    first part complete - frontend

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,32 @@
+// import { useAuthContext } from "../../context/AuthContext";
+// import { extractTime } from "../../utils/extractTime";
+// import useConversation from "../../zustand/useConversation";
+
+const Message = ({ message }) => {
+  // const { authUser } = useAuthContext();
+  // const { selectedConversation } = useConversation();
+  // const fromMe = message.senderId === authUser._id;
+  // const formattedTime = extractTime(message.createdAt);
+  // const chatClassName = fromMe ? "chat-end" : "chat-start";
+  // const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  // const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+  // const shakeClass = message.shouldShake ? "shake" : "";
+
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS chat bubble component" src="" />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white bg-blue-500`}>
+        Hey what's up!!
+      </div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        Time
+      </div>
+    </div>
+  );
+};
+export default Message;
---
+++ frontend/src/components/messages/MessageInput.jsx
@@ -0,0 +1,19 @@
+import { BsSend } from "react-icons/bs";
+
+const MessageInput = () => {
+	return (
+		<form className='px-5 my-3 relative'>
+			<div className='w-full'>
+				<input
+					type='text'
+					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+					placeholder='Send a message'
+				/>
+				<button type='submit' className='absolute inset-y-0 end-6 flex items-center pe-3'>
+					<BsSend />
+				</button>
+			</div>
+		</form>
+	);
+};
+export default MessageInput;
